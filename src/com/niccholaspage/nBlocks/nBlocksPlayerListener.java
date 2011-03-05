6e43ac0b0d9094b1fc5a4663615dc219e6adf7ed
niccholaspage <niccholaspage@gmail.com>
2011-03-05 13:49:23 -0500

    Player Pickup

--- src/com/niccholaspage/nBlocks/nBlocksPlayerListener.java
+++ src/com/niccholaspage/nBlocks/nBlocksPlayerListener.java
@@ -1,10 +1,19 @@
 package com.niccholaspage.nBlocks;
-
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 
 public class nBlocksPlayerListener extends PlayerListener {
 	 public static nBlocks plugin;
 	 public nBlocksPlayerListener(nBlocks instance) {
 		 plugin = instance;
 		 }
+	 public void onPlayerPickupItem(PlayerPickupItemEvent event){
+		 Player player = event.getPlayer();
+		 Item item = event.getItem();
+		  if (!(nBlocks.Permissions.has(player, "nBlocks." + item.getItemStack().getType().name().toLowerCase()))){
+			  event.setCancelled(true);
+		  }
+	 }
 }
